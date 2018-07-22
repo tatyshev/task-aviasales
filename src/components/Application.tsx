ce0b34b6250ebec2a7c55bd5218a7386258a88bb
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-22 16:06:27 +0300

    header added

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 export default () => (
   <div className='b-application'>
-    Application
+    <div className='b-application__header'>
+      <div className='b-container'>
+        <img
+          className='b-application__logo'
+          src='logo.svg'
+          alt='Aviasales'
+          width='82'
+          height='88'
+        />
+      </div>
+    </div>
+
+    <div className='b-application__body'>
+      <div className='b-container'>
+        Application
+      </div>
+    </div>
   </div>
 );
