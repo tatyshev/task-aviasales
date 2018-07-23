9df3ba5a3d71343bce19f7bf3a91c0c7a0af256e
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-23 09:36:53 +0300

    init transfers

--- src/components/Transfers.tsx
+++ src/components/Transfers.tsx
@@ -1,7 +1,23 @@
+import noop from 'lodash.noop';
 import React from 'react';
 
-export default () => (
+interface IProps {
+  value?: number[];
+  onChange?: () => void;
+}
+
+export default ({ value, onChange = noop }: IProps) => (
   <div className='b-transfers'>
-    Transfers
+    <div className='b-transfers__item'>
+      <div className='b-transfers__label'>Все</div>
+    </div>
+
+    <div className='b-transfers__item'>
+      <div className='b-transfers__label'>1 Пересадка</div>
+    </div>
+
+    <div className='b-transfers__item'>
+      <div className='b-transfers__label'>2 Пересадка</div>
+    </div>
   </div>
 );
