113d3a21a75957a3270245e62ecd5e591b7830db
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-25 23:20:59 +0300

    currency markup

--- src/components/Transfers.tsx
+++ src/components/Transfers.tsx
@@ -8,8 +8,14 @@ interface IProps {
 
 export default ({ value, onChange = noop }: IProps) => (
   <div className='b-transfers'>
-    <div className='b-transfers__item'>
+    <div className='b-transfers__item b-transfers__item--active'>
       <div className='b-transfers__label'>Все</div>
+      <button className='b-transfers__only'>Только</button>
+    </div>
+
+    <div className='b-transfers__item b-transfers__item--active'>
+      <div className='b-transfers__label'>Без пересадок</div>
+      <button className='b-transfers__only'>Только</button>
     </div>
 
     <div className='b-transfers__item'>
@@ -18,6 +24,10 @@ export default ({ value, onChange = noop }: IProps) => (
 
     <div className='b-transfers__item'>
       <div className='b-transfers__label'>2 Пересадка</div>
+    </div>
+
+    <div className='b-transfers__item'>
+      <div className='b-transfers__label'>3 Пересадки</div>
     </div>
   </div>
 );
