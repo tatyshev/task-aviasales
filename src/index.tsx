63cdc95b164b441607def5e9ee719a5539a8ecd1
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-31 01:08:16 +0300

    add fetch polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import 'normalize.css';
 import Application from '@/components/Application';
 import React from 'react';
 import { render } from 'react-dom';
+import 'whatwg-fetch';
 
 render(<Application />, document.getElementById('app'));
