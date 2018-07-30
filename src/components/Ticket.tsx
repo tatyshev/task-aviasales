e41948d616f9e2a8f926ad8e1ee859010731f276
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-07-31 00:27:58 +0300

    hardcoded currencies

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,16 +1,43 @@
 import Route from '@/components/Route';
-import { ITicket } from '@/state/interfaces';
+import { INullStr, IState, ITicket } from '@/state/interfaces';
 import React from 'react';
+import { connect } from 'react-redux';
 
 interface ITicketProps {
   ticket: ITicket;
+  currency?: INullStr;
 }
 
-const numberWithSpaces = (x: number) => {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+interface ICurrencies {
+  [key: string]: {
+    rate: number,
+    chartCode: number,
+  };
+}
+
+// Hardcoded currencies, these values are taken for an example
+const CURRENCIES: ICurrencies = {
+  RUB: { rate: 1, chartCode: 8381 },
+  USD: { rate: 0.016, chartCode: 36 },
+  EUR: { rate: 0.014, chartCode: 8364 },
 };
 
-export default ({ ticket }: ITicketProps) => (
+const numberWithSpaces = (x: number | string) => {
+  return Number(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+};
+
+const renderPrice = (price: number, currency?: INullStr) => {
+  const { rate, chartCode } = CURRENCIES[currency || 'RUB'];
+  const result = (price * rate).toFixed();
+
+  return (
+    <div className='b-ticket__price'>
+      за {numberWithSpaces(result)} {String.fromCharCode(chartCode)}
+    </div>
+  );
+};
+
+const Ticket = ({ ticket, currency }: ITicketProps) => (
   <div className='b-widget b-ticket'>
     <div className='b-ticket__offer'>
       <img
@@ -24,7 +51,7 @@ export default ({ ticket }: ITicketProps) => (
 
       <button className='b-ticket__button'>
         <div className='b-ticket__buy'>Купить</div>
-        <div className='b-ticket__price'>за {numberWithSpaces(ticket.price)} &#8381;</div>
+        {renderPrice(ticket.price, currency)}
       </button>
     </div>
 
@@ -33,3 +60,9 @@ export default ({ ticket }: ITicketProps) => (
     </div>
   </div>
 );
+
+const mapStateToProps = (state: IState) => ({
+  currency: state.currency,
+});
+
+export default connect(mapStateToProps)(Ticket);
